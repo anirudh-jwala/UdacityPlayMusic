dfb604e514b6b06732f548d4ce71b48e8a36342f
anirudh-jwala <anirudhsai2@gmail.com>
2018-07-18 20:52:37 +0530

    Commit

--- app/src/main/java/com/example/anirudh/udacityplaymusic/TopArtistsActivity.java
+++ app/src/main/java/com/example/anirudh/udacityplaymusic/TopArtistsActivity.java
@@ -18,16 +18,16 @@ public class TopArtistsActivity extends AppCompatActivity{
         setContentView(R.layout.song_list);
 
         ArrayList<Song> songs = new ArrayList<Song>();
-        songs.add(new Song("Diamonds", "Rihanna","03:45",R.drawable.Diamonds));
-        songs.add(new Song("Delicate", "Taylor Swift","03:23",R.drawable.Delicate));
-        songs.add(new Song("God's Plan","Drake","04:21", R.drawable.GodsPlan));
-        songs.add(new Song("Lose Yourself","Eminem","05:02",R.drawable.Lose_Yourself));
+        songs.add(new Song("Diamonds", "Rihanna","03:45",R.drawable.diamonds));
+        songs.add(new Song("Delicate", "Taylor Swift","03:23",R.drawable.delicate));
+        songs.add(new Song("God's Plan","Drake","04:21", R.drawable.godsplan));
+        songs.add(new Song("Lose Yourself","Eminem","05:02",R.drawable.loseyourself));
         songs.add(new Song("Girls Like You","Maroon 5","04:14",R.drawable.girlslikeyou));
         songs.add(new Song("Sorry","Justin Bieber","03:32",R.drawable.sorry));
         songs.add(new Song("Naked Truth","Sean Paul","05:02",R.drawable.nakedtruth));
         songs.add(new Song("One Kiss","Calvin Harris","04:34",R.drawable.onekiss));
         songs.add(new Song("We Don't Talk Anymore","Charlie Puth","04:24",R.drawable.we_don_t_talk_anymore));
-        songs.add(new Song("Capital Letters","Hailee Steinfeild","04:51",R.drawable.Capital_Letters));
+        songs.add(new Song("Capital Letters","Hailee Steinfeild","04:51",R.drawable.capitalletters));
 
         SongAdapter itemsAdapter = new SongAdapter(this, songs);
 
--- app/src/main/java/com/example/anirudh/udacityplaymusic/TopSongsActivity.java
+++ app/src/main/java/com/example/anirudh/udacityplaymusic/TopSongsActivity.java
@@ -19,15 +19,15 @@ public class TopSongsActivity extends AppCompatActivity {
 
         ArrayList<Song> songs = new ArrayList<Song>();
         songs.add(new Song("Magenta Riddim", "DJ Snake", "03:14", R.drawable.riddim));
-        songs.add(new Song("Back To You", "Selena Gomez", "03:27", R.drawable.Back_to_You));
-        songs.add(new Song("Havana", "Camila Cabello", "03:36", R.drawable.Havana));
-        songs.add(new Song("Mi Gente", "Willy William", "03:30",R.drawable.Mi_Gente));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.Wolves));
-        songs.add(new Song("Dusk Till Dawn", "Zayn Malik", "04:28", R.drawable.Dusk_Till_Dawn));
+        songs.add(new Song("Back To You", "Selena Gomez", "03:27", R.drawable.backtoyou));
+        songs.add(new Song("Havana", "Camila Cabello", "03:36", R.drawable.havana));
+        songs.add(new Song("Mi Gente", "Willy William", "03:30",R.drawable.migente));
+        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.wolves));
+        songs.add(new Song("Dusk Till Dawn", "Zayn Malik", "04:28", R.drawable.dusktilldawn));
         songs.add(new Song("Animals", "Maroon 5", "05:21", R.drawable.animals));
-        songs.add(new Song("Somebody", "The Chainsmokers", "03:53", R.drawable.Somebody));
-        songs.add(new Song("Mad Love", "Sean Paul", "04:24", R.drawable.Mad_Love));
-        songs.add(new Song("Something Just Like This", "The Chainsmokers", "04:48", R.drawable.sorry));
+        songs.add(new Song("Somebody", "The Chainsmokers", "03:53", R.drawable.somebody));
+        songs.add(new Song("Mad Love", "Sean Paul", "04:24", R.drawable.madlove));
+        songs.add(new Song("Something Just Like This", "The Chainsmokers", "04:48", R.drawable.sjlt));
 
         SongAdapter itemsAdapter = new SongAdapter(this, songs);
 
