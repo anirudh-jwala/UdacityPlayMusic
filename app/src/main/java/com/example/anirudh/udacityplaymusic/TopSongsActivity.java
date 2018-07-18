ee131c73d57a578d2b5d00f8b8c1a925e83ac8a2
anirudh-jwala <anirudhsai2@gmail.com>
2018-07-18 20:42:05 +0530

    Commit

--- app/src/main/java/com/example/anirudh/udacityplaymusic/TopSongsActivity.java
+++ app/src/main/java/com/example/anirudh/udacityplaymusic/TopSongsActivity.java
@@ -18,16 +18,16 @@ public class TopSongsActivity extends AppCompatActivity {
         setContentView(R.layout.song_list);
 
         ArrayList<Song> songs = new ArrayList<Song>();
-        songs.add(new Song("Magenta Riddim", "DJ Snake", "03:14", R.drawable.ic_launcher_background));
-        songs.add(new Song("Back To You", "Selena Gomez", "03:27", R.drawable.ic_launcher_background));
-        songs.add(new Song("Havana", "Camila Cabello", "03:36", R.drawable.ic_launcher_background));
-        songs.add(new Song("Mi Gente", "Willy William", "03:30",R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
-        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.ic_launcher_background));
+        songs.add(new Song("Magenta Riddim", "DJ Snake", "03:14", R.drawable.riddim));
+        songs.add(new Song("Back To You", "Selena Gomez", "03:27", R.drawable.Back_to_You));
+        songs.add(new Song("Havana", "Camila Cabello", "03:36", R.drawable.Havana));
+        songs.add(new Song("Mi Gente", "Willy William", "03:30",R.drawable.Mi_Gente));
+        songs.add(new Song("Wolves", "Selena Gomez", "03:18", R.drawable.Wolves));
+        songs.add(new Song("Dusk Till Dawn", "Zayn Malik", "04:28", R.drawable.Dusk_Till_Dawn));
+        songs.add(new Song("Animals", "Maroon 5", "05:21", R.drawable.animals));
+        songs.add(new Song("Somebody", "The Chainsmokers", "03:53", R.drawable.Somebody));
+        songs.add(new Song("Mad Love", "Sean Paul", "04:24", R.drawable.Mad_Love));
+        songs.add(new Song("Something Just Like This", "The Chainsmokers", "04:48", R.drawable.sorry));
 
         SongAdapter itemsAdapter = new SongAdapter(this, songs);
 
